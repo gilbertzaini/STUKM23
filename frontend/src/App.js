037fb93601f463877c8d993f260b0dc41e55249e
xenoz32 <prayoga.wijaya3223@gmail.com>
2023-11-20 22:35:55 +0700

    iniperbaikanBentrok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import * as React from "react";
-<<<<<<< Updated upstream
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-=======
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import ConnectionTest from "./components/connectionTest";
 import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
->>>>>>> Stashed changes
 import "./App.css";
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Sponsor from "./Pages/Sponsor";
 import Index from "./Pages/Index";
 import Ukm from "./Pages/Ukm";
-<<<<<<< Updated upstream
 import { Observer } from "./helper/observer";
 import { useEffect } from "react";
-=======
 // IMMPORT MEDPAR PAGE DISINI
->>>>>>> Stashed changes
 
 function App() {
   useEffect(() => {
--- frontend/src/Pages/Index.js
+++ frontend/src/Pages/Index.js
@@ -7,15 +7,8 @@ import Footer from "../components/Footer";
 const Index = () => {
   return (
     <>
-<<<<<<< Updated upstream
-    <Navbar/>
-    <Logo/>
-    <VotingForm/>
-    <Footer />
-=======
     <Logo />
     <VotingForm id="votingForm"/>
->>>>>>> Stashed changes
     </>
   )
 }

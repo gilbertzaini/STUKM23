97d2b954de7d950a57b7821e9cbe663e5ac3fa4c
Gilbert Zaini <gilbertzaini03@gmail.com>
2023-11-26 23:59:40 +0700

    feat(fe): teaser render condition

--- frontend/src/Pages/Index.js
+++ frontend/src/Pages/Index.js
@@ -1,21 +1,25 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import VotingForm from "../components/VotingForm";
 import Logo from "../components/Logo";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Teaser from "../components/Teaser";
 import { Observer } from "../helper/observer";
+import Teaser from "../components/Teaser";
 
 const Index = () => {
   useEffect(() => {
     Observer();
   }, []);
 
+  // Check if the user agent contains "Firefox"
+  const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+
   return (
     <>
       <Logo />
       <VotingForm id="votingForm" />
-      <Teaser />
+      {isFirefox ? null : <Teaser />}
+      {/* <Teaser/> */}
     </>
   );
 };
